737691da5750342680e187edd10d1f5337cd1b98
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-11-06 14:41:11 -0500

    modified reverseString.js to complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    const reverseArray = [];
 
+    for (let i = 0; i < words.length; i++) {
+        reverseArray.unshift(words[i]);
+    }
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
