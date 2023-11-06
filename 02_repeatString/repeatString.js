b85d4ddd5efda776912585cf714cf9f034b4e150
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-11-06 12:47:29 -0500

    Modified respeatString.js to complete exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    let concatString = "";
 
+    if (timesToRepeat < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < timesToRepeat; i++) {
+        concatString += stringToRepeat;
+    }
+    return concatString;
 };
 
 // Do not edit below this line
