f0f89c6571989f490e1a17bcfb7476539bca7ada
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-11-14 05:18:36 -0500

    Modified tempConversion.js and passed all required tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC = (tempInF - 32) * 5 / 9;
+  let roundedTempInC = Math.round(tempInC * 10) / 10;
+  return roundedTempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let tempInF = (tempInC * 9 / 5 + 32);
+  let roundedTempInF = Math.round(tempInF * 10) / 10;
+  return roundedTempInF;
 };
 
 // Do not edit below this line
