efe290ba95af6292d9d24afd3e4563b0d8b791df
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-11-14 01:45:55 -0500

    modified sumAll.js and executed all tests as passing.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    let sum = 0;
+    if (begin < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(begin) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    if (begin > end) {
+        let temp = begin;
+        begin = end;
+        end = temp;
+    }
+
+    for (let i = begin; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
