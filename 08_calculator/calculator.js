3e733c08c23f5a67b33568c46d3e90193f6ccb3b
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-12-01 14:09:11 -0500

    Modified calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  numbers.forEach((number) => {
+    sum += number;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let product = 1;
+  numbers.forEach((number) => {
+    product *= number;
+  });
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let power = Math.pow(num1, num2);
+	return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let num = number;
+	let factorial = 1;
+
+  while(num > 0) {
+    factorial *= num;
+    num--;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
