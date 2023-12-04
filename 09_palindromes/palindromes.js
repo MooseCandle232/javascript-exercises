e7af169e3fe417e4f5b86a11032e2e051cb472e4
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-12-04 13:57:29 -0500

    modified palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    // let reverseWords = words.split("").reverse().join("")
+    // return reverseWords === words;
+    const reverseArray = [];
+    let justLetters = [];
 
+    for (let i = 0; i < words.length; i++) {
+        justLetters = words.toLowerCase().match(/[A-Za-z0-9]/g);
+        reverseArray.unshift(justLetters[i]);
+    }
+    let wordForward = justLetters.join("");
+    let wordReversed = reverseArray.join("");
+
+    return wordReversed === wordForward;
 };
 
 // Do not edit below this line
