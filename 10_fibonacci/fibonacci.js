e67129ee6276538acf88db3c732eda2c400ac71e
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-12-04 17:56:04 -0500

    modified fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let startNum = 0;
+    let startNum2 = 1;
+    let fibonacciSum = 0;;
 
+    if (member < 0) return "OOPS"
+
+    for (let i = 0; i < member; i++) {
+        fibonacciSum = startNum + startNum2;
+        startNum2 = startNum;
+        startNum = fibonacciSum;
+    }
+
+    return fibonacciSum;
 };
 
 // Do not edit below this line
